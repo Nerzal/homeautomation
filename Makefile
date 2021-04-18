d651e2b7df1a5bd574bfd655cba12991f4ebed35
Nerzal <info@noobygames.de>
2021-04-18 16:32:34 +0200

    refactor login component

--- Makefile
+++ Makefile
@@ -8,5 +8,7 @@ dashboard:
 	xcopy /f ".\dashboard\wasm.js" ".\html"
 	xcopy /f ".\dashboard\index.html" ".\html"
 	xcopy /f ".\dashboard\styles\general.css" ".\html"
+	mkdir ".\html\assets"
+	xcopy /f ".\assets" ".\html\assets"
 	tinygo build -o ".\html\wasm.wasm" -opt=s --no-debug ".\dashboard\wasm.go"
 	go run ".\api\main.go"
--- dashboard/views/login/login.go
+++ dashboard/views/login/login.go
@@ -34,6 +34,72 @@ func (s *Service) RenderLogin() {
 	headerComponent := doc.CreateElement("div").
 		SetClass("header")
 
+	loginComponent := s.createLoginComponent()
+
+	content.AppendChildren(headerComponent, loginComponent)
+
+	doc.GetElementById("username").Focus()
+
+}
+
+func (s *Service) onLogin(this js.Value, args []js.Value) interface{} {
+	userInput := input.FromElement(s.userInput).Value()
+	passwordInput := input.FromElement(s.passwordInput).Value()
+
+	println("user:", userInput)
+	println("password:", passwordInput)
+
+	// TODO: Let the Server(API) validate the credentials
+
+	if userInput != userName {
+		handleInvalidCredentials()
+		return nil
+	}
+
+	if passwordInput != password {
+		handleInvalidCredentials()
+		return nil
+	}
+
+	// Goroutine is needed, as blocking operations like this are not allowed inside of async javascript handlers
+	// This event handler is called from javascript -> gluecode -> Go
+	go func() {
+		s.events <- Event{UserName: userInput}
+	}()
+
+	return nil
+}
+
+func handleInvalidCredentials() {
+	userInput := doc.GetElementById("username")
+	err := userInput.AppendClass("invalid-input")
+	if err != nil {
+		println("failed to append failed-input class:", err.Error())
+	}
+
+	passwordInput := doc.GetElementById("password")
+	err = passwordInput.AppendClass("invalid-input")
+	if err != nil {
+		println("failed to append failed-input class:", err.Error())
+	}
+
+	invalidInputText := doc.GetElementById("invalid-input-text")
+	if !invalidInputText.IsNull() {
+		return
+	}
+
+	loginComponent := doc.GetElementById("login-component")
+
+	hintText := doc.CreateElement("p").
+		SetInnerHTML("invalid username or password!").
+		SetClass("invalid-input-text", "center").
+		SetId("invalid-input-text")
+
+	loginComponent.AppendChildBr(hintText)
+
+}
+
+func (s *Service) createLoginComponent() *tinydom.Element {
 	loginComponent := doc.CreateElement("div").
 		SetClass("login-component").
 		SetId("login-component")
@@ -66,66 +132,8 @@ func (s *Service) RenderLogin() {
 
 	loginComponent.AppendChildrenBr(userNameLabel, userInput, passwordLabel, passwordInput, loginButton)
 
-	content.AppendChildren(headerComponent, loginComponent)
-
-	doc.GetElementById("username").Focus()
-
 	s.userInput = userInput
 	s.passwordInput = passwordInput
-}
 
-func (s *Service) onLogin(this js.Value, args []js.Value) interface{} {
-	userInput := input.FromElement(s.userInput).Value()
-	passwordInput := input.FromElement(s.passwordInput).Value()
-
-	println("user:", userInput)
-	println("password:", passwordInput)
-
-	// TODO: Let the Server(API) validate the credentials
-
-	if userInput != userName {
-		handleInvalidCredentials()
-		return nil
-	}
-
-	if passwordInput != password {
-		handleInvalidCredentials()
-		return nil
-	}
-
-	go func() {
-		s.events <- Event{UserName: userInput}
-	}()
-
-	return nil
-}
-
-func handleInvalidCredentials() {
-	// tinydom.GetWindow().Alert("Invalid username or password")
-	userInput := doc.GetElementById("username")
-	err := userInput.AppendClass("invalid-input")
-	if err != nil {
-		println("failed to append failed-input class:", err.Error())
-	}
-
-	passwordInput := doc.GetElementById("password")
-	err = passwordInput.AppendClass("invalid-input")
-	if err != nil {
-		println("failed to append failed-input class:", err.Error())
-	}
-
-	invalidInputText := doc.GetElementById("invalid-input-text")
-	if !invalidInputText.IsNull() {
-		return
-	}
-
-	loginComponent := doc.GetElementById("login-component")
-
-	hintText := doc.CreateElement("p").
-		SetInnerHTML("invalid username or password!").
-		SetClass("invalid-input-text", "center").
-		SetId("invalid-input-text")
-
-	loginComponent.AppendChildBr(hintText)
-
+	return loginComponent
 }
